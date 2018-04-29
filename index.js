b9e9cafcfb60d60b8f21c0af131dd7d80acfa9db
Sean Huang <hyprul@ucla.edu>
2018-04-29 23:53:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ function logWhisper(string){
 
 
 function sayHiToGrandma(string){
-  if(string.toUpperCase() === uppercase){
+  if(string.toUpperCase() === string){
     return "YES INDEED!"
   }
-  else if(string.toLowerCase() === string)
+  else if(string.toLowerCase() === string){
+    return "I can't hear you!"
+  }
+  else if(string === "I love you, Grandma."){
+    return "I love you, too"
+  }
   
 }
